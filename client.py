54230956d12f9b9ab4757324d923fd95600289b1
Natalia Sanabria Forero <n.sanabria@uniandes.edu.co>
2022-03-17 17:55:52 -0500

    Update client and server so they can extend threads

--- client.py
+++ client.py
@@ -1,21 +1,42 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-
-"""
 # Import socket module
-import socket            
- 
-# Create a socket object
-s = socket.socket()        
- 
-# Define the port on which you want to connect
-port = 12345               
- 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
- 
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
-# close the connection
-s.close() 
+import socket
+
+
+def Main():
+	# local host IP '127.0.0.1'
+	host = '127.0.0.1'
+
+	# Define the port on which you want to connect
+	port = 12345
+
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+	# connect to server on local computer
+	s.connect((host,port))
+
+	# message you send to server
+	message = "shaurya says geeksforgeeks"
+	while True:
+
+        # ACÁ ES NECESARIO GESTIONAR LA PETICIÓN DEL SERVICIO
+		# message sent to server
+		s.send(message.encode('ascii'))
+
+		# message received from server
+		data = s.recv(1024)
+
+		# print the received message
+		# here it would be a reverse of sent message
+		print('Received from the server :',str(data.decode('ascii')))
+
+		# ask the client whether he wants to continue
+		ans = input('\nDo you want to continue(y/n) :')
+		if ans == 'y':
+			continue
+		else:
+			break
+	# close the connection
+	s.close()
+
+if __name__ == '__main__':
+	Main()
